25df69ca60e00d029f4a406613fea48968872db5
tuyentvph25898.github.io <tuyentvph25898@fpt.edu.vn>
2023-10-09 23:07:11 +0700

    apidiachi

--- S_Shop_Server/app.js
+++ S_Shop_Server/app.js
@@ -6,7 +6,9 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var apiRouter = require('./routes/api')
+var apiRouter = require('./routes/api');
+var addressRouter = require('./routes/address.route');
+
 
 var app = express();
 
@@ -23,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/api',apiRouter);
+app.use('/address',addressRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
