d8217e4b289838487163726c0a1bee27e1a6c505
vanhai0k <hainvph26725@fpt.edu.vn>
2023-10-28 09:20:25 +0700

    hai_push_FILTER

--- S_Shop_Server/models/product.model.js
+++ S_Shop_Server/models/product.model.js
@@ -12,7 +12,6 @@ const productSchema = new db.mongoose.Schema(
         },
       ],
       description: { type: String, required: true },
-      // note: { type: String, required: false },
       image: { type: String, required: true },
     },
     { collection: 'products' }
